00d6d76fe6cf281b7b3cb0215ce6520ed4965aad
Xilarie1 <martin8.kodehode@gmail.com>
2025-10-09 11:02:11 +0200

    finished server.js for today

--- server.js
+++ server.js
@@ -4,10 +4,35 @@ const express = require("express");
 
 const app = express();
 
-// routes
-app.get("/", (req, res) => {
-  res.send("Goodbye world!");
+// Serve static files (CSS, JS, images) from the root directory
+app.use(express.static(__dirname));
+
+// Routes for HTML pages
+app.get(/^\/$|\/index(\.html)?$/, (req, res) => {
+  res.sendFile(path.join(__dirname, "view", "index.html"));
 });
+
+app.get("/new-page.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "view", "new-page.html"));
+});
+
+// Redirect old pages
+app.get(/^\/$|\/old-page(\.html)?$/, (req, res) => {
+  res.redirect(301, "/new-page.html");
+});
+
+app.get("/www-page.html", (req, res) => {
+  res.redirect(301, "/");
+});
+
+app.get(/\/*/, (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "view", "404.html"));
+});
+
+// Catch-all route for 404
+// app.use((req, res) => {
+//   res.status(404).sendFile(path.join(__dirname, "view", "404.html"));
+// });
 
 const PORT = process.env.PORT || 3500;
 
